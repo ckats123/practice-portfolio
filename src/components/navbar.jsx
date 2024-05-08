ed3b8038767b6d136387bd1aa5e94b589e62212c
Sophia Katsuno <sophia.katsuno@gmail.com>
2024-05-07 20:56:32 -0700

    partial navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 const Navbar = () => {
   return (
-    <div className=''>Navbar'</div>
- )
-}
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 ">
+      {/* LOGO */}
+      <div className="">
+        <Link href="/" className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center">
+          <span className="text-white mr-1">Sophia</span>
+          <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center ">.dev</span>
+        </Link>
+      </div>
+      {/*  RESPONSIVE MENU */}
+      <div className="">2</div>
+    </div>
+  );
+};
 
 export default Navbar;
