12f0481a9e4cc1fcf922ab07e07a3da5c702c374
Sophia Katsuno <sophia.katsuno@gmail.com>
2024-05-09 12:18:26 -0700

    rough draft of navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,44 @@
 import Link from "next/link";
 
+const links = [
+  { url: "/", name: "Home" },
+  { url: "/about", name: "About" },
+  { url: "/projects", name: "Projects" },
+  { url: "/contact", name: "Contact" },
+];
+
 const Navbar = () => {
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 ">
       {/* LOGO */}
       <div className="">
-        <Link href="/" className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center">
+        <Link
+          href="/"
+          className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
+        >
           <span className="text-white mr-1">Sophia</span>
-          <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center ">.dev</span>
+          <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center ">
+            .dev
+          </span>
         </Link>
       </div>
       {/*  RESPONSIVE MENU */}
-      <div className="">2</div>
+      <div className="">
+        {/* {MENU BUTTON}  */}
+        <button className="w-10 h-8 flex flex-col justify-between z-50 relative">
+          <div className="w-10 h-1 bg-black rounded"></div>
+          <div className="w-10 h-1 bg-black rounded"></div>
+          <div className="w-10 h-1 bg-black rounded"></div>
+        </button>
+        {/* {MENU LIST} */}
+        <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items=center justify-center gap-8 text:4xl">
+          {links.map((link) => (
+            <Link href={link.url} key={link.title}>
+          {link.title}
+            </Link>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
