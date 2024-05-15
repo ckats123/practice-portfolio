93e1e141cac0568ce3204efc2c804050bc978fb2
Sophia Katsuno <sophia.katsuno@gmail.com>
2024-05-14 20:04:39 -0700

    incorporate framer motion to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import NavLink from "./navLink";
-// import { motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 const links = [
   { url: "/", title: "Home" },
@@ -125,7 +125,7 @@ const Navbar = () => {
           className="w-10 h-8 flex flex-col justify-between z-50 relative"
           onClick={() => setOpen((prev) => !prev)}
         >
-          {/* <motion.div
+          <motion.div
             variants={topVariants}
             animate={open ? "opened" : "closed"}
             className="w-10 h-1 bg-black rounded origin-left"
@@ -139,7 +139,7 @@ const Navbar = () => {
             variants={bottomVariants}
             animate={open ? "opened" : "closed"}
             className="w-10 h-1 bg-black rounded origin-left"
-          ></motion.div> */}
+          ></motion.div>
         </button>
         {/* MENU LIST */}
         {open && (
