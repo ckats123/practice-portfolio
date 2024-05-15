d59effa728fc072db930ea0f97e98c611aa26b1e
Sophia Katsuno <sophia.katsuno@gmail.com>
2024-05-14 20:19:47 -0700

    fix alignment of name and description

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,11 +4,9 @@ const Homepage = () => {
   return (
     <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
       {/* IMAGE CONTAINER */}
-      <div className="h-1/2 lg:h-full lg:w-1/2 relative">
-       
-      </div>
+      <div className="h-1/2 lg:h-full lg:w-1/2 relative"></div>
       {/* TEXT CONTAINER */}
-      <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+      <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center xl:items-start lg:items-start justify-center">
         {/* TITLE */}
         <h1 className="text-4xl md:text-6xl font-bold">
           Sophia Katsuno
